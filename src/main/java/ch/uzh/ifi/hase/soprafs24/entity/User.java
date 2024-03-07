194c430deadc35024d4538340dbfd3c525c4c19f
Gian-Luca <gian-luca.fuehrer@uzh.ch>
2024-03-07 23:40:30 +0100

    fix for prewritten tests to pass

--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/User.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/User.java
@@ -38,7 +38,7 @@ public class User implements Serializable {
   @Column(nullable = false)
   private UserStatus status;
 
-  @Column(nullable = false)
+  @Column(nullable = true)
   private LocalDate creationDate;
 
   @Column(nullable = true)
--- src/main/java/ch/uzh/ifi/hase/soprafs24/service/UserService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/UserService.java
@@ -3,8 +3,6 @@ package ch.uzh.ifi.hase.soprafs24.service;
 import ch.uzh.ifi.hase.soprafs24.constant.UserStatus;
 import ch.uzh.ifi.hase.soprafs24.entity.User;
 import ch.uzh.ifi.hase.soprafs24.repository.UserRepository;
-import ch.uzh.ifi.hase.soprafs24.rest.dto.UserGetDTO;
-import ch.uzh.ifi.hase.soprafs24.rest.mapper.DTOMapper;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
